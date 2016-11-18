548f53c6f7e6ef09c4d07bdf17d46ee8679c6791
Kyle <kylelam@me.com>
2016-11-17 19:41:43 -0800

    adding some show/hide function

--- ninja.js
+++ ninja.js
@@ -1,1 +1,15 @@
 // ninja
+
+function showPageByClass(className){
+    document.getElementsByClassName(className).removeAttribute("hidden");
+}
+function showPageById(id){
+    document.getElementById(id).removeAttribute("hidden");
+}
+
+function hidePageByclass(className){
+    document.getElementsByClassName(className).setAttribute("hidden","");
+}
+function hidePageById(id){
+    document.getElementById(id).setAttribute("hidden","");
+}
